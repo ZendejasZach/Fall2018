c8368f00f2fbacdb2a1ca5b39cee272ca00dfe75
Zach Zendejas <ZAX1527@gmail.com>
2018-10-11 23:02:12 -0600

    Outline for Sammich site

---
+++ CIT261/2.JavaScriptObjects/sammich.js
@@ -0,0 +1,5 @@
+// first create Sammich object
+// Add type of Sammich to Sammich object from user selection
+// Add user selected bread to Sammich object 
+// include price with above functions.
+// total function call here.
